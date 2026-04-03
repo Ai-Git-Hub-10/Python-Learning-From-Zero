95ef5cd11c5cbee0ff5c72a3308c4a98b8e6185b
Shayan Shah <shayanshah.edu.pk@gmail.com>
2026-04-03 18:09:33 +0500

    Variable_and_datatypes

---
+++ Day_1/Variable_and_datatypes.py
@@ -0,0 +1,16 @@
+# We will cover all the concepts about variale and data types 
+#First read the notes.txt file 
+
+#Variables
+
+name = "Shayan Shah"
+age = 23
+college = "XYZ"
+phone = 0123445667
+
+print(name)
+print(age)
+print(college)
+print(phone)
+
+#in above [name,age,college and phone are variables]

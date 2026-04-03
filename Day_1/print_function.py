90b95bb5626a12a0f02021222105cdd0a89066e7
Shayan Shah <shayanshah.edu.pk@gmail.com>
2026-04-03 18:15:03 +0500

    New info added

--- Day_1/print_function.py
+++ Day_1/print_function.py
@@ -3,4 +3,9 @@
 
 print("Hello World")
 
-# it will print ('Hello World')
+print("I love my country.")
+
+print("My name is Shayan Shah.")
+
+# it will print all the content or strings inside Double quotes""
+# print() function automatically add new line when call.

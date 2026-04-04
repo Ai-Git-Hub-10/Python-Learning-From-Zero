deb2152794ee97d12944445e717678d47022709d
Shayan Shah <shayanshah.edu.pk@gmail.com>
2026-04-04 12:20:42 +0500

    Add practice problems

--- Day_1_Variable_&_Datatypes/Practice.py
+++ Day_1_Variable_&_Datatypes/Practice.py
@@ -15,20 +15,105 @@ print("He is",age,"year old.")
 print("His hieght =",hieght)
 print("He is a student =",is_student)
 
+#------------------------------------------------------#
 
-# Taking user input
-# we can take input from user by using keyword input()
-# Input() function always return string data type
+# Practice problem 2:
+# Swapping values:
 
-num=input("Enter number: ")
+a = 20
+b = 30
 
-# type convertion
-num = int(num) # From string to integer
+print(a,b)  # print 20 , 30
 
-num1 = float(num) # From string to flaot 
+# now swap them
 
-# print output 
-print("Your enter number are =",num)
+a  , b = b , a
+print(a,b)  # print 30 , 20
 
-print("Your entered number in flaot =",num1)
- 
+#-------------------------------------------------------#
+
+# Practice problem 3:
+# Update present varaible:
+# First creat a variable then update it 
+
+age = 23
+print(age)      # print 23
+
+# Update value of age by 5
+# This is also called string catenation.
+# simply (+) operator to add or update a variable value
+
+updated_age = age + 5
+print(updated_age)      # print 28
+
+#--------------------------------------------------------#
+
+# Practice problem 4:
+# Multiple assiagnment.
+# In python we can also value to varaible at bulk .
+
+a , b , c = 10 , 20 , 30 
+print(a,b,c)    # print 10 , 20 , 30 
+
+# Swapping :
+
+a , b , c = b , c , a # print 20 , 30 , 10 
+print(a,b,c)
+
+#---------------------------------------------------------#
+
+# Practice problem 5:
+# Dynamic typing 
+# Use type() function to check its type
+
+data = 100 
+print (data)
+print(type(data)) # integer type
+
+# Also called type conversion
+# Both output will be same but having different data types
+
+data = str(data)
+print (data)  
+print(type(data))   # string type
+
+#---------------------------------------------------------#
+
+# Practice problem 6:
+# Copying and reference
+
+a = 40
+b = a       # Copying a to b
+
+print(a,b)  # print 40 , 40 
+
+# update variable
+
+a = 60 
+print(a)        # print 60
+print(b)        # print 40
+
+#----------------------------------------------------------#
+
+# Practice problem 7:
+# Take input from user 
+# Using input function
+# Asking name and favorite color store it and display it
+
+name = input ("Enter your name: ")
+favorite_color = input ("Enter your favorite color name: ")
+
+print("Your name is : ",name)       # display user input e.g. jhon
+print("Your favorite color is : ",favorite_color)       # display input e.g Black
+
+#-----------------------------------------------------------#
+
+# Practice problem 8:
+# Adding two variables
+
+first_name = input ("Enter your first name: ")
+last_name = input ("Enter your last name: ")
+
+full_name = first_name +" "+ last_name
+
+print("Your name is = ",full_name) # display full_name of user e.g. Thomas shelby
--- Day_1_Variable_&_Datatypes/print_input_and_type_function.py
+++ Day_1_Variable_&_Datatypes/print_input_and_type_function.py
@@ -18,14 +18,21 @@ print("My name is Shayan Shah.")
 # Input function by default always return string data.
 
 name=input("Enter your name: ")
+age=input("Enter your age: ")
 print(name) # return any thing entered by user 
 
 
 # Type function ():
 
 # In python type() function is used to check the data type of varaible.
+# We simply use  print(type(varaible))
 print(type(name)) # output: class str
 
 num = 3
 print(type(num)) # output: class int
 
+pi = 3.14 
+print(type(pi)) # output: class flaot 
+
+l = [1,2,3]
+print(type(l)) # output: class list
